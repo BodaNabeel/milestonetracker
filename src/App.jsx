3df3c5c4e679c82c96ca5a5fa66f95b3c8ab89c9
Nabeel Boda <needtoexplore01@gmail.com>
2023-03-09 11:26:00 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 function App() {
-  // create function to generate random number above 10 
-function generateRandomNumber() {
-  let generateRandomNumber = Math.random()*10
-  return generateRandomNumber
-}
-console.log(generateRandomNumber(), 1)
-  
-  return <h1>first commit.. </h1>
+ return "hello world"
 }
 
 export default App
+
+
+
+
