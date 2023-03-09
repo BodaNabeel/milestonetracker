e72c0661f99a5a8e09ef69bc2a0669a6c3096293
Nabeel Boda <needtoexplore01@gmail.com>
2023-03-09 11:48:01 +0530

    -

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+import "./style.css";
 
-
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
