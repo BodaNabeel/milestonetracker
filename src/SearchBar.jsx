7bc565d90a873a5e63316ca381afe99da8e32083
Nabeel Boda <needtoexplore01@gmail.com>
2023-03-11 12:43:44 +0530

    -

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
+import useMediaQuery from '@mui/material/useMediaQuery';
 import TextField from "@mui/material/TextField";
-
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from '@mui/material/InputAdornment';
 export default function SearchBar() {
   const [searchQuery, setSearchQuery] = React.useState("");
   const [data, setData] = React.useState(0);
+  const responsiveSearchBar = useMediaQuery('(min-width:1020px)');
+  console.log(responsiveSearchBar, "Responsive Search Bar")
   const handleEnter = (event) => {
     if (event.charCode === 13 || event.keyCode == 13) {
       setData(searchQuery);
@@ -24,16 +28,25 @@ export default function SearchBar() {
   return (
     <Box
       sx={{
-        width: 500,
+        width: "100%",
         maxWidth: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <TextField
         onKeyDown={handleEnter}
         onChange={handleSearch}
-        placeholder="search"
+        placeholder="search movie/book/game"
         fullWidth
         id="fullWidth"
+        InputProps={{
+          startAdornment: <InputAdornment position="start"><SearchIcon/></InputAdornment>,
+        }}
+        sx={{
+          width:responsiveSearchBar ? "50%" : "80%"
+        }}
       />
     </Box>
   );
