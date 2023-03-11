30afd07cf96917cbdcdc83ef52cc745bbfec2eda
Nabeel Boda <needtoexplore01@gmail.com>
2023-03-11 12:48:51 +0530

    -

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,21 +1,17 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import useMediaQuery from '@mui/material/useMediaQuery';
+import useMediaQuery from "@mui/material/useMediaQuery";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
-import InputAdornment from '@mui/material/InputAdornment';
+import InputAdornment from "@mui/material/InputAdornment";
 export default function SearchBar() {
-  const [searchQuery, setSearchQuery] = React.useState("");
   const [data, setData] = React.useState(0);
-  const responsiveSearchBar = useMediaQuery('(min-width:1020px)');
-  console.log(responsiveSearchBar, "Responsive Search Bar")
-  const handleEnter = (event) => {
-    if (event.charCode === 13 || event.keyCode == 13) {
-      setData(searchQuery);
-    }
-  };
+  const responsiveSearchBar = useMediaQuery("(min-width:1020px)");
   const handleSearch = (event) => {
-    setSearchQuery(event.target.value);
+    if (event.charCode === 13 || event.keyCode == 13) {
+      setData(event.target.value);
+      event.target.value = "";
+    }
   };
   React.useEffect(() => {
     if (data === 0) {
@@ -36,16 +32,19 @@ export default function SearchBar() {
       }}
     >
       <TextField
-        onKeyDown={handleEnter}
-        onChange={handleSearch}
+        onKeyDown={handleSearch}
         placeholder="search movie/book/game"
         fullWidth
         id="fullWidth"
         InputProps={{
-          startAdornment: <InputAdornment position="start"><SearchIcon/></InputAdornment>,
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
         }}
         sx={{
-          width:responsiveSearchBar ? "50%" : "80%"
+          width: responsiveSearchBar ? "50%" : "80%",
         }}
       />
     </Box>
