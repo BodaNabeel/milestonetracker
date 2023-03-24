a62cd08b7123360f91067f9b311b0de73ce5d695
Nabeel Boda <needtoexplore01@gmail.com>
2023-03-24 23:42:15 +0530

    BooksRender Component Build

--- src/DataDisplay.jsx
+++ src/DataDisplay.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { DataContext } from "./data/DataStore";
 import ScrollableTabsButtonForce from "./Scrollable";
 
 function DataDisplay() {
-  return <ScrollableTabsButtonForce />;
+  const { movies, books } = useContext(DataContext);
+  function BooksRender({ books }) {
+    if (books.length === 0) {
+      return <h1>NO DATA FOUND</h1>;
+    } else {
+      return (
+        <>
+          {books.items.map((element, index) => {
+            return <h1 key={index}>{element.volumeInfo.title}</h1>;
+          })}
+        </>
+      );
+    }
+  }
+
+  return (
+    <>
+      <ScrollableTabsButtonForce />
+      {<BooksRender books={books} />}
+    </>
+  );
 }
 
 export default DataDisplay;
