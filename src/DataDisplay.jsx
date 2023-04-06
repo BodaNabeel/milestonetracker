8a139097ca98f2a2f18fc4515ecdd7d620cf4887
Nabeel Boda <needtoexplore01@gmail.com>
2023-04-06 22:48:23 +0530

    -

--- src/DataDisplay.jsx
+++ src/DataDisplay.jsx
@@ -4,6 +4,7 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  Grid,
   Typography,
 } from "@mui/material";
 import React, { useContext, useEffect } from "react";
@@ -28,14 +29,33 @@ function DataDisplay() {
         <>
           {books.items.map((element, index) => {
             return (
-              <Card key={index} sx={{ maxWidth: 350, height: 350 }}>
+              <Card
+                key={index}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  border: "2px solid red",
+                  maxWidth: "max-content",
+                  width: "80%"
+                }}
+              >
                 <CardMedia
                   component="img"
                   alt={"thumbnail of " + element.volumeInfo.title}
-                  sx={{ width: "100%", height: "auto" }}
                   image={element.volumeInfo.imageLinks.smallThumbnail}
+                  sx={{
+                    width: "100%",
+                    // height: "380px",
+                    objectFit: "contain",
+                  }}
                 />
-                <CardContent>
+
+                <CardContent
+                  sx={{
+                    width: "160px"
+                    // flexGrow: 1,
+                  }}
+                >
                   <Typography gutterBottom variant="h5" component="div">
                     {element.volumeInfo.title}
                   </Typography>
@@ -59,6 +79,7 @@ function DataDisplay() {
     <>
       <ScrollableTabsButtonForce />
       <div className="book-container">{<BooksRender books={books} />}</div>
+      {/* <Grid container>{<BooksRender books={books} />}</Grid> */}
       <div className="movie-conatiner">{<MoviesRender movies={movies} />}</div>
       <div className="series-container">{<SeriesRender series={series} />}</div>
     </>
