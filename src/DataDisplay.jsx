99e83f17d2bc3c7d58ff1e8a3a23e53fe71a8c64
Nabeel Boda <needtoexplore01@gmail.com>
2023-04-07 00:24:20 +0530

    -

--- src/DataDisplay.jsx
+++ src/DataDisplay.jsx
@@ -24,52 +24,66 @@ function DataDisplay() {
     if (books.length === 0) {
       return <h1>NO DATA FOUND</h1>;
     } else {
-      console.log(books);
       return (
         <>
-          {books.items.map((element, index) => {
-            return (
-              <Card
-                key={index}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  border: "2px solid red",
-                  maxWidth: "max-content",
-                  width: "80%"
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  alt={"thumbnail of " + element.volumeInfo.title}
-                  image={element.volumeInfo.imageLinks.smallThumbnail}
-                  sx={{
-                    width: "100%",
-                    // height: "380px",
-                    objectFit: "contain",
-                  }}
-                />
+          <Grid container direction="row" spacing={2} m={4}>
+            {books.items.map((element, index) => {
+              return (
+                <Grid item key={index}>
+                  <Card
+                    sx={{
+                      width: "100%",
+                      display: "flex",
+                      height: "460px",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      alt={"thumbnail of " + element.volumeInfo.title}
+                      image={element.volumeInfo.imageLinks.smallThumbnail}
+                      sx={{
+                        width: "100%",
+                        maxWidth: "200px",
+                        height: "300px",
+                        objectFit: "fill",
+                        alignSelf: "center",
+                      }}
+                    />
 
-                <CardContent
-                  sx={{
-                    width: "160px"
-                    // flexGrow: 1,
-                  }}
-                >
-                  <Typography gutterBottom variant="h5" component="div">
-                    {element.volumeInfo.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {element.volumeInfo.subtitle}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-              </Card>
-            );
-          })}
+                    <CardContent
+                      sx={{
+                        width: "100%",
+                        maxWidth: "200px",
+                        flexGrow: 1,
+                      }}
+                    >
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                        sx={{}}
+                        className="text-truncate"
+                      >
+                        {element.volumeInfo.title}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="text.secondary"
+                        className="text-truncate"
+                      >
+                        {element.volumeInfo.subtitle}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Share</Button>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
         </>
       );
     }
@@ -78,8 +92,24 @@ function DataDisplay() {
   return (
     <>
       <ScrollableTabsButtonForce />
-      <div className="book-container">{<BooksRender books={books} />}</div>
-      {/* <Grid container>{<BooksRender books={books} />}</Grid> */}
+      {/* <div className="book-container">{<BooksRender books={books} />}</div> */}
+      {/* <Grid container direction="row">
+        <Grid item>
+          <BooksRender books={books} />
+        </Grid>
+      </Grid> */}
+      {/* <Grid container direction="row">
+        {books.items.map((element, index) => (
+          <Grid item key={index}>
+            <BooksRender books={books} />
+          </Grid>
+        ))}
+      </Grid> */}
+      <Grid container direction="row">
+        <Grid item>
+          <BooksRender books={books} />
+        </Grid>
+      </Grid>
       <div className="movie-conatiner">{<MoviesRender movies={movies} />}</div>
       <div className="series-container">{<SeriesRender series={series} />}</div>
     </>
