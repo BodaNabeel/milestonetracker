7b1acc1647a071aa4eb5682ac674ccff84ec3996
Nabeel Boda <needtoexplore01@gmail.com>
2023-04-09 16:18:52 +0530

    wrote clean code

--- src/component/Scrollable.jsx
+++ src/component/Scrollable.jsx
@@ -4,51 +4,30 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { useMediaQuery } from "@mui/material";
 
-function MobileViewTabs() {
-  const [value, setValue] = React.useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Tabs
-      value={value}
-      onChange={handleChange}
-      allowScrollButtonsMobile
-      scrollButtons
-      variant="scrollable"
-    >
-      <Tab label="All" />
-      <Tab label="Movies" />
-      <Tab label="Series" />
-      <Tab label="Books" />
-      <Tab label="Games" />
-    </Tabs>
-  );
-}
-
-function DesktopViewTabs() {
-  const [value, setValue] = React.useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  return (
-    <Tabs value={value} onChange={handleChange} centered>
-      <Tab label="All" />
-      <Tab label="Movies" />
-      <Tab label="Series" />
-      <Tab label="Books" />
-      <Tab label="Games" />
-    </Tabs>
-  );
-}
-
-export default function ScrollableTabsButtonForce() {
+export default function ScrollableTabsButtonForce({
+  labelNumber,
+  setLabelNumber,
+}) {
   const vw = useMediaQuery("(max-width: 600px)");
-
+  const handleChange = (event, newValue) => {
+    setLabelNumber(newValue);
+  };
   return (
     <Box sx={{ maxWidth: { s: 320, md: "100%" }, bgcolor: "background.paper" }}>
-      {vw ? <MobileViewTabs /> : <DesktopViewTabs />}
+      <Tabs
+        value={labelNumber}
+        onChange={handleChange}
+        allowScrollButtonsMobile={vw ? true : false}
+        centered={vw ? false : true}
+        scrollButtons={vw ? true : false}
+        variant={vw ? "scrollable" : ""}
+      >
+        <Tab value={0} label="All" />
+        <Tab value={1} label="Books" />
+        <Tab value={2} label="Movies" />
+        <Tab value={3} label="Series" />
+        <Tab value={4} label="Games" />
+      </Tabs>
     </Box>
   );
 }
