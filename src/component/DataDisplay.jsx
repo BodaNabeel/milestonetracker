b1f2212e0a09886228df3fc213c2637a03d20e7c
Nabeel Boda <needtoexplore01@gmail.com>
2023-04-09 16:18:41 +0530

    Conditional rendering of cards

--- src/component/DataDisplay.jsx
+++ src/component/DataDisplay.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Scrollable from "./Scrollable";
-
 import BookCard from "./card/BookCard";
 import MovieCard from "./card/MovieCard";
 import SeriesCard from "./card/SeriesCard";
 function DataDisplay() {
+  const [labelNumber, setLabelNumber] = useState(0);
+  const ConditionalRenderingOfData = function () {
+    switch (labelNumber) {
+      case 0:
+        return (
+          <>
+            <BookCard />
+            <MovieCard />
+            <SeriesCard />
+          </>
+        );
+      case 1:
+        return <BookCard />;
+      case 2:
+        return <MovieCard />;
+      case 3:
+        return <SeriesCard />;
+    }
+  };
   return (
     <>
-      <Scrollable />
-      <BookCard />
-      <MovieCard />
-      <SeriesCard/>
+      <Scrollable setLabelNumber={setLabelNumber} labelNumber={labelNumber} />
+      <ConditionalRenderingOfData />
     </>
   );
 }
