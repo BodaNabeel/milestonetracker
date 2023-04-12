24a0969383f1df5727c18175957d4b97fac160cc
Nabeel Boda <needtoexplore01@gmail.com>
2023-04-13 02:40:10 +0530

    Fixed Bug on rendering everytime

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ function App() {
       ) : null}
       {responsiveSearchBar ? null : <Divider />}
 
-      {query ? <DataDisplay /> : null}
-      <DoughnutChart/>
+      {query ? <DataDisplay /> : <DoughnutChart/>}
+      
     </>
   );
 }
--- src/component/DoughnutChart.jsx
+++ src/component/DoughnutChart.jsx
@@ -1,40 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Doughnut } from "react-chartjs-2";
 import Chart from "chart.js/auto";
-function DoughnutChart() {
-//   ChartJS.register(ArcElement, Legend,Tick);
-  const DoughnutChart = () => {
-    const data = {
-      labels: ["Completed", "Pending"],
-      datasets: [
-        {
-          data: [4,9],
-          backgroundColor: "rgba(75,192,192,0.2)",
-          borderColor: "rgba(75,192,192,1)",
-          borderWidth: 1,
-        },
-        
-      ],
-    };
 
-    const options = {
-      cutout: "85%",
-        plugins: {
-          legend: {
-            display: false,
-            onClick: null,
-          },
-        },
-    };
-    return (
-      <div id={1} className="chart-container">
-        <Doughnut data={data} options={options} />
-      </div>
-    );
+function DoughnutChart() {
+ 
+  const data = {
+    labels: ["Completed", "Pending"],
+    datasets: [
+      {
+        data: [4, 9],
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+        borderWidth: 1,
+      },
+    ],
   };
+
+  const options = {
+    cutout: "85%",
+    plugins: {
+      legend: {
+        display: false,
+        onClick: null,
+      },
+    },
+  };
+
   return (
     <div>
-      <DoughnutChart />
+      <div id={1} className="chart-container">
+        <Doughnut ref={chartRef} data={data} options={options} />
+      </div>
     </div>
   );
 }
