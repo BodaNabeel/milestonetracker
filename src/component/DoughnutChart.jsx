1a9bebb8c7d25a3ee4307f2fbf98e4f998987e25
Nabeel Boda <needtoexplore01@gmail.com>
2023-04-13 02:47:37 +0530

    removed ref{}

--- src/component/DoughnutChart.jsx
+++ src/component/DoughnutChart.jsx
@@ -3,12 +3,11 @@ import { Doughnut } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 
 function DoughnutChart() {
- 
   const data = {
     labels: ["Completed", "Pending"],
     datasets: [
       {
-        data: [4, 9],
+        data: [4, 20],
         backgroundColor: "rgba(75,192,192,0.2)",
         borderColor: "rgba(75,192,192,1)",
         borderWidth: 1,
@@ -29,7 +28,7 @@ function DoughnutChart() {
   return (
     <div>
       <div id={1} className="chart-container">
-        <Doughnut ref={chartRef} data={data} options={options} />
+        <Doughnut data={data} options={options} />
       </div>
     </div>
   );
