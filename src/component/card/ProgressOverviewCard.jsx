52057c5552d4cce9106c459a5d916243363bafa6
Nabeel Boda <needtoexplore01@gmail.com>
2023-04-14 02:38:10 +0530

    rendering data dynamically

--- src/component/card/ProgressOverviewCard.jsx
+++ src/component/card/ProgressOverviewCard.jsx
@@ -1,34 +1,31 @@
-import React, { useContext } from 'react'
-import { DataContext } from '../../data/DataContext'
-import DoughnutChart from '../DoughnutChart'
-
+import React, { useContext } from "react";
+import { DataContext } from "../../data/DataContext";
+import DoughnutChart from "../DoughnutChart";
 export default function ProgressOverviewCard() {
-    const {storedBooks, storedMovies, storedSeries, storedGames} = useContext(DataContext)
-    console.log(storedBooks.length)
+  const { CompletedPendingData } = useContext(DataContext);
+  const keys = Object.keys(CompletedPendingData);
   return (
-    <div className='ProgressOverviewCard-Container'>
-        <div className="ProgressOverviewCard">
-            <h1 className="ProgressOverviewCard-title">Books</h1>
+    <div className="ProgressOverviewCard-Container">
+      {keys.map((el) => {
+        return (
+          <div
+            key={el}
+            className="ProgressOverviewCard"
+            style={{ border: "1px solid red" }}
+          >
+            <h1 className="ProgressOverviewCard-title">
+              {CompletedPendingData[el].title}
+            </h1>
             <div className="ProgressOverviewCard-content">
-                <DoughnutChart />
-                <button className="ProgressOverviewCard-btn">Details</button>
+              <DoughnutChart
+                completed={CompletedPendingData[el].completed}
+                pending={CompletedPendingData[el].pending}
+              />
+              <button className="ProgressOverviewCard-btn">Details</button>
             </div>
-        </div>
-        <div className="ProgressOverviewCard">
-            <h1 className="ProgressOverviewCard-title">Movies</h1>
-            <div className="ProgressOverviewCard-content">
-                <DoughnutChart />
-                <button className="ProgressOverviewCard-btn">Details</button>
-            </div>
-        </div>
-        <div className="ProgressOverviewCard">
-            <h1 className="ProgressOverviewCard-title">Series</h1>
-            <div className="ProgressOverviewCard-content">
-                <DoughnutChart />
-                <button className="ProgressOverviewCard-btn">Details</button>
-            </div>
-        </div>
-
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
