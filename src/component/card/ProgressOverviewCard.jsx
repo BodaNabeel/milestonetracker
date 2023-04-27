4a6a41f771d7422d47f6e289562d3b02bd332932
Nabeel Boda <needtoexplore01@gmail.com>
2023-04-27 11:46:48 +0530

    added boilerplate

--- src/component/card/ProgressOverviewCard.jsx
+++ src/component/card/ProgressOverviewCard.jsx
@@ -5,24 +5,62 @@ export default function ProgressOverviewCard() {
   const { CompletedPendingData } = useContext(DataContext);
   const keys = Object.keys(CompletedPendingData);
   return (
-    <div className="ProgressOverviewCard-Container">
+    <div
+      className="ProgressOverviewCard-Container"
+      style={
+        {
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          
+        
+        }
+      }
+    >
       {keys.map((el) => {
         return (
           <div
             key={el}
             className="ProgressOverviewCard"
-            style={{ border: "1px solid red" }}
+            style={{
+              border: "1px solid red",
+              height: "450px",
+              width: "360px",
+              margin: "15px 0",
+              display: "flex",
+              flexDirection: "column",
+            }}
           >
-            <h1 className="ProgressOverviewCard-title">
+            <h1
+              className="ProgressOverviewCard-title"
+              style={{
+                height: "2%",
+              }}
+            >
               {CompletedPendingData[el].title}
             </h1>
-            <div className="ProgressOverviewCard-content">
-              <DoughnutChart
-                completed={CompletedPendingData[el].completed}
-                pending={CompletedPendingData[el].pending}
-              />
-              <button className="ProgressOverviewCard-btn">Details</button>
-            </div>
+            {/* <div
+              className="ProgressOverviewCard-content"
+              style={
+                {
+                  // height: "mac-co"
+                }
+              }
+            > */}
+            <DoughnutChart
+              completed={CompletedPendingData[el].completed}
+              pending={CompletedPendingData[el].pending}
+            />
+            <button
+              className="ProgressOverviewCard-btn"
+              style={{
+                alignSelf: "flex-end",
+                marginRight: "12px",
+              }}
+            >
+              Details
+            </button>
+            {/* </div> */}
           </div>
         );
       })}
