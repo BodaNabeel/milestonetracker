4912b4a8e37784e5d092ceebb070e9a1ee81422a
Nabeel Boda <needtoexplore01@gmail.com>
2023-05-01 12:56:50 +0530

    displaying text at center

--- src/component/DoughnutChart.jsx
+++ src/component/DoughnutChart.jsx
@@ -3,7 +3,28 @@ import { Doughnut } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 
-function DoughnutChart({ completed, pending }) {
+function DoughnutChart({ completed, pending, total }) {
+ const completionPercentage = Math.round((completed/total)*100)
+  const plugins = [{
+    beforeDraw: function(chart) {
+     var width = chart.width,
+         height = chart.height,
+         ctx = chart.ctx;
+         ctx.restore();
+        //  var fontSize = (height / 160).toFixed(2);
+        var fontSize = 2;
+         ctx.font = fontSize + "em sans-serif";
+         ctx.textBaseline = "top";
+        //  var text = `(${completed}(${completed+pending}))*100`,
+        // var text = completed/(completed+pending)*100,
+        var text = `${completionPercentage}%`,
+         textX = Math.round((width - ctx.measureText(text).width) / 2),
+         textY = height / 2;
+         ctx.fillText(text, textX, textY);
+         ctx.save();
+    } 
+  }]
+
   Chart.register(ChartDataLabels);
   const data = {
     labels: ["Completed", "Pending"],
@@ -13,8 +34,6 @@ function DoughnutChart({ completed, pending }) {
         backgroundColor: ["#4464A8", "#92A2D0"],
         hoverBackgroundColor: ["#4464A8", "#92A2D0"],
         // TODO: use radius property to set it's height and length
-
-        // TODO: show completion %age in the center
         // ex: radius: "40%"
         borderWidth: 1,
       },
@@ -22,7 +41,7 @@ function DoughnutChart({ completed, pending }) {
   };
 
   const options = {
-    cutout: "85%",
+    cutout: "80%",
     plugins: {
       legend: {
         display: false,
@@ -41,7 +60,7 @@ function DoughnutChart({ completed, pending }) {
   return (
     <div>
       <div id={1} className="chart-container">
-        <Doughnut data={data} options={options} />
+        <Doughnut data={data} options={options} plugins={plugins} />
       </div>
     </div>
   );
