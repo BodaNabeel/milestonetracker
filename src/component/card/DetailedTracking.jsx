6e26e44317fa8eb5f4ef50ef4954b90f564e38a4
Nabeel Boda <needtoexplore01@gmail.com>
2023-05-04 11:12:25 +0530

    Rendering content

--- src/component/card/DetailedTracking.jsx
+++ src/component/card/DetailedTracking.jsx
@@ -1,14 +1,130 @@
 import React from "react";
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { DataContext } from "../../data/DataContext";
+import BookCard from "./BookCard";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+// TODO: Build the structure for this compoenent which renders for all movies, books, series along all functionalitites like mark as completed, incompleted, remove from the list
+// TODO: Then look into the error related to sending cookie in a cross-site
+
+function DataRender({ data, parameter }) {
+  if (data.length === 0) {
+    return <h1>NO DATA FOUND</h1>;
+  } else {
+    return (
+      <>
+        <Grid
+          container
+          direction="row"
+          spacing={2}
+          sx={{
+            justifyContent: useMediaQuery("(max-width:760px)") ? "center" : "",
+            margin: "10px auto",
+            width: "80%",
+          }}
+        >
+          {data.map((element, index) => {
+            return (
+              <Grid item key={index} style={{}}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    height: "460px",
+                    width: "230px",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    alt={
+                      parameter === "Books"
+                        ? `thumbnail of ${element.volumeInfo.title}`
+                        : parameter === "Movies" || parameter === "Series"
+                        ? `thumbnail of ${element.original_title}`
+                        : ""
+                    }
+                    image={
+                      parameter === "Books"
+                        ? element.volumeInfo.imageLinks === undefined
+                          ? ""
+                          : element.volumeInfo.imageLinks.thumbnail
+                        : parameter === "Movies" || parameter === "Series"
+                        ? `https://image.tmdb.org/t/p/original${element.backdrop_path}`
+                        : ""
+                    }
+                    sx={{
+                      width: "100%",
+                      maxWidth: "200px",
+                      height: "300px",
+                      objectFit: "fill",
+                      alignSelf: "center",
+                    }}
+                  />
+                  <CardContent
+                    sx={{
+                      width: "100%",
+                      maxWidth: "200px",
+                      flexGrow: 1,
+                    }}
+                  >
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+                      component="div"
+                      sx={{}}
+                      className="text-truncate"
+                    >
+                      {parameter === "Books"
+                        ? element.volumeInfo.title
+                        : parameter === "Movies"
+                        ? element.original_title
+                        : parameter === "Series"
+                        ? element.name
+                        : ""}
+                    </Typography>
+
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      className="text-truncate-para"
+                    >
+                      {parameter === "Books"
+                        ? element.volumeInfo.subtitle
+                        : parameter === "Movies" || "Series"
+                        ? element.overview
+                        : ""}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </>
+    );
+  }
+}
 
 function DetailedTracking() {
-    // Getting the params which will be used to condtionally render data in UI
+  const { storedMovies, storedBooks, storedSeries } = useContext(DataContext);
   const routeParams = useParams();
-  return (
-    <>
-      <div>DetailedTracking {routeParams.id} </div>
-    </>
-  );
+  const routeId = routeParams.id;
+
+  if (routeId === "Books") {
+    return <DataRender data={storedBooks} parameter={routeId} />;
+  } else if (routeId === "Series") {
+    return <DataRender data={storedSeries} parameter={routeId} />;
+  } else if (routeId === "Movies") {
+    return <DataRender data={storedMovies} parameter={routeId} />;
+  }
 }
 
 export default DetailedTracking;
