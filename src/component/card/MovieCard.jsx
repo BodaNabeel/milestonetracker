744bae52c1d662821ea7e1cfeadcafc98011a1e9
Nabeel Boda <needtoexplore01@gmail.com>
2023-05-06 12:27:07 +0530

    fixed re-rendering bug

--- src/component/card/MovieCard.jsx
+++ src/component/card/MovieCard.jsx
@@ -14,91 +14,86 @@ function MovieCard() {
   const { movies, setStoredMovies, storedMovies } = useContext(DataContext);
   const identifier = movies.results;
   const imageUrl = `https://image.tmdb.org/t/p/original`;
-  function MovieRender({ movies }) {
-    if (movies.length == 0) {
-      return <h1>NO DATA FOUND</h1>;
-    } else {
-      return (
-        <>
-          <Grid
-            container
-            direction="row"
-            spacing={2}
-            sx={{
-              justifyContent: useMediaQuery("(max-width:760px)")
-                ? "center"
-                : "",
-              margin: "10px auto",
-              width: "80%",
-            }}
-          >
-            {movies.results.map((element, index) => {
-              return (
-                <Grid item key={index}>
-                  <Card
+  if (movies.length == 0) {
+    return <h1>NO DATA FOUND</h1>;
+  } else {
+    return (
+      <>
+        <Grid
+          container
+          direction="row"
+          spacing={2}
+          sx={{
+            justifyContent: useMediaQuery("(max-width:760px)") ? "center" : "",
+            margin: "10px auto",
+            width: "80%",
+          }}
+        >
+          {movies.results.map((element, index) => {
+            return (
+              <Grid item key={index}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    height: "460px",
+                    width: "230px",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    alt={"Thumbnail of " + element.original_title}
+                    image={imageUrl + element.backdrop_path}
                     sx={{
-                      display: "flex",
-                      height: "460px",
-                      width: "230px",
-                      flexDirection: "column",
+                      width: "100%",
+                      maxWidth: "200px",
+                      height: "300px",
+                      objectFit: "fill",
+                      alignSelf: "center",
+                    }}
+                  />
+
+                  <CardContent
+                    sx={{
+                      width: "100%",
+                      maxWidth: "200px",
+                      flexGrow: 1,
                     }}
                   >
-                    <CardMedia
-                      component="img"
-                      alt={"Thumbnail of " + element.original_title}
-                      image={imageUrl + element.backdrop_path}
-                      sx={{
-                        width: "100%",
-                        maxWidth: "200px",
-                        height: "300px",
-                        objectFit: "fill",
-                        alignSelf: "center",
-                      }}
-                    />
-
-                    <CardContent
-                      sx={{
-                        width: "100%",
-                        maxWidth: "200px",
-                        flexGrow: 1,
-                      }}
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+                      component="div"
+                      sx={{}}
+                      className="text-truncate"
                     >
-                      <Typography
-                        gutterBottom
-                        variant="h6"
-                        component="div"
-                        sx={{}}
-                        className="text-truncate"
-                      >
-                        {element.original_title}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="text.secondary"
-                        className="text-truncate-para"
-                      >
-                        {element.overview}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <BookmarkButton
-                        uniquePara={element.id}
-                        token={index}
-                        setData={setStoredMovies}
-                        identifier={identifier}
-                        data={storedMovies}
-                      />
-                    </CardActions>
-                  </Card>
-                </Grid>
-              );
-            })}
-          </Grid>
-        </>
-      );
-    }
+                      {element.original_title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      className="text-truncate-para"
+                    >
+                      {element.overview}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <BookmarkButton
+                      uniquePara={element.id}
+                      token={index}
+                      setData={setStoredMovies}
+                      identifier={identifier}
+                      data={storedMovies}
+                    />
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </>
+    );
   }
-  return <MovieRender movies={movies} />;
 }
 
 export default MovieCard;
--- src/component/card/SeriesCard.jsx
+++ src/component/card/SeriesCard.jsx
@@ -15,91 +15,86 @@ function SeriesCard() {
   const { series, setStoredSeries, storedSeries } = useContext(DataContext);
   const identifier = series.results;
   const imageUrl = `https://image.tmdb.org/t/p/original`;
-  function SeriesRender({ series }) {
-    if (series.length == 0) {
-      return <h1>NO DATA FOUND</h1>;
-    } else {
-      return (
-        <>
-          <Grid
-            container
-            direction="row"
-            spacing={2}
-            sx={{
-              justifyContent: useMediaQuery("(max-width:760px)")
-                ? "center"
-                : "",
-              margin: "10px auto",
-              width: "80%",
-            }}
-          >
-            {series.results.map((element, index) => {
-              return (
-                <Grid item key={index}>
-                  <Card
+  if (series.length == 0) {
+    return <h1>NO DATA FOUND</h1>;
+  } else {
+    return (
+      <>
+        <Grid
+          container
+          direction="row"
+          spacing={2}
+          sx={{
+            justifyContent: useMediaQuery("(max-width:760px)") ? "center" : "",
+            margin: "10px auto",
+            width: "80%",
+          }}
+        >
+          {series.results.map((element, index) => {
+            return (
+              <Grid item key={index}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    height: "460px",
+                    width: "230px",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    alt={"Thumbnail of " + element.original_title}
+                    image={imageUrl + element.backdrop_path}
                     sx={{
-                      display: "flex",
-                      height: "460px",
-                      width: "230px",
-                      flexDirection: "column",
+                      width: "100%",
+                      maxWidth: "200px",
+                      height: "300px",
+                      objectFit: "fill",
+                      alignSelf: "center",
+                    }}
+                  />
+
+                  <CardContent
+                    sx={{
+                      width: "100%",
+                      maxWidth: "200px",
+                      flexGrow: 1,
                     }}
                   >
-                    <CardMedia
-                      component="img"
-                      alt={"Thumbnail of " + element.original_title}
-                      image={imageUrl + element.backdrop_path}
-                      sx={{
-                        width: "100%",
-                        maxWidth: "200px",
-                        height: "300px",
-                        objectFit: "fill",
-                        alignSelf: "center",
-                      }}
-                    />
-
-                    <CardContent
-                      sx={{
-                        width: "100%",
-                        maxWidth: "200px",
-                        flexGrow: 1,
-                      }}
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+                      component="div"
+                      sx={{}}
+                      className="text-truncate"
                     >
-                      <Typography
-                        gutterBottom
-                        variant="h6"
-                        component="div"
-                        sx={{}}
-                        className="text-truncate"
-                      >
-                        {element.name}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="text.secondary"
-                        className="text-truncate-para"
-                      >
-                        {element.overview}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <BookmarkButton
-                        token={index}
-                        uniquePara={element.id}
-                        setData={setStoredSeries}
-                        identifier={identifier}
-                        data={storedSeries}
-                      />
-                    </CardActions>
-                  </Card>
-                </Grid>
-              );
-            })}
-          </Grid>
-        </>
-      );
-    }
+                      {element.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      className="text-truncate-para"
+                    >
+                      {element.overview}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <BookmarkButton
+                      token={index}
+                      uniquePara={element.id}
+                      setData={setStoredSeries}
+                      identifier={identifier}
+                      data={storedSeries}
+                    />
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </>
+    );
   }
-  return <SeriesRender series={series} />;
 }
 
 export default SeriesCard;
