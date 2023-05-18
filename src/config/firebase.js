3e7fb3a621a035029e72b46bdfff8f7914b4b5dd
Nabeel Boda <needtoexplore01@gmail.com>
2023-05-18 17:55:08 +0530

    -

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -29,49 +29,49 @@ const dbRef = sRef(getDatabase());
 setPersistence(auth, browserLocalPersistence);
 
 const writeBookEl = (userId, bookEl, loaded) => {
-  if (loaded) {
+  if (userId && loaded) {
     update(sRef(db, `users/${userId}/data`), {
       book: bookEl.length !== 0 ? bookEl : null,
     });
   }
 };
 const writeMovieEl = (userId, movieEl, loaded) => {
-  if (loaded) {
+  if (userId && loaded) {
     update(sRef(db, `users/${userId}/data`), {
       movie: movieEl.length !== 0 ? movieEl : null,
     });
   }
 };
 const writeSeriesEl = (userId, seriesEl, loaded) => {
-  if (loaded) {
+  if (userId && loaded) {
     update(sRef(db, `users/${userId}/data`), {
       series: seriesEl.length !== 0 ? seriesEl : null,
     });
   }
 };
 const writeCompletedBooks = (userId, completedBooks, loaded) => {
-  if (loaded) {
+  if (userId && loaded) {
     update(sRef(db, `users/${userId}/data`), {
       completed_books: completedBooks.length !== 0 ? completedBooks : null,
     });
   }
 };
 const writeCompletedMovies = (userId, completedMovies, loaded) => {
-  if (loaded) {
+  if (userId && loaded) {
     update(sRef(db, `users/${userId}/data`), {
       completed_movies: completedMovies.length !== 0 ? completedMovies : null,
     });
   }
 };
 const writeCompletedSeries = (userId, completedSeries, loaded) => {
-  if (loaded) {
+  if (userId && loaded) {
     update(sRef(db, `users/${userId}/data`), {
       completed_series: completedSeries.length !== 0 ? completedSeries : null,
     });
   }
 };
 const writeClickedButtonsList = (userId, clickedButtonsList, loaded) => {
-  if (loaded) {
+  if (userId && loaded) {
     update(sRef(db, `users/${userId}/data`), {
       clicked_buttons_list:
         clickedButtonsList.length !== 0 ? clickedButtonsList : null,
@@ -86,31 +86,36 @@ const readUserContent = (
   updateCompletedMovies,
   updateCompletedSeries,
   setLoaded,
-  updateClickedButtonsList
+  updateClickedButtonsList,
+  userId
 ) => {
-  const userID = localStorage.getItem("userIdentification");
+  if (userId) {
+    get(child(dbRef, `users/${userId}/data`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        data.book !== undefined ? updateBook(data.book) : updateBook([]);
+        data.movie !== undefined ? updateMovie(data.movie) : updateMovie([]);
+        data.series !== undefined
+          ? updateSeries(data.series)
+          : updateSeries([]);
+        data.completed_books !== undefined
+          ? updateCompletedBooks(data.completed_books)
+          : updateCompletedBooks([]);
+        data.completed_movies !== undefined
+          ? updateCompletedMovies(data.completed_movies)
+          : updateCompletedMovies([]);
+        data.completed_series !== undefined
+          ? updateCompletedSeries(data.completed_series)
+          : updateCompletedSeries([]);
+        data.clicked_buttons_list !== undefined
+          ? updateClickedButtonsList(data.clicked_buttons_list)
+          : updateClickedButtonsList([]);
+      }
+      setLoaded(true);
+    });
+  }
+  // const userID = localStorage.getItem("userIdentification");
   console.log("mounted");
-  get(child(dbRef, `users/${userID}/data`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      const data = snapshot.val();
-      data.book !== undefined ? updateBook(data.book) : updateBook([]);
-      data.movie !== undefined ? updateMovie(data.movie) : updateMovie([]);
-      data.series !== undefined ? updateSeries(data.series) : updateSeries([]);
-      data.completed_books !== undefined
-        ? updateCompletedBooks(data.completed_books)
-        : updateCompletedBooks([]);
-      data.completed_movies !== undefined
-        ? updateCompletedMovies(data.completed_movies)
-        : updateCompletedMovies([]);
-      data.completed_series !== undefined
-        ? updateCompletedSeries(data.completed_series)
-        : updateCompletedSeries([]);
-      data.clicked_buttons_list !== undefined
-        ? updateClickedButtonsList(data.clicked_buttons_list)
-        : updateClickedButtonsList([]);
-    }
-    setLoaded(true);
-  });
 };
 
 export {
