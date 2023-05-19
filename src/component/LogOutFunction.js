84d8ebd1e3844aad79f80ec739c30f0b90a737df
Nabeel Boda <needtoexplore01@gmail.com>
2023-05-19 11:05:26 +0530

    removed comments

--- src/component/LogOutFunction.js
+++ src/component/LogOutFunction.js
@@ -3,9 +3,7 @@ import { getAuth } from "firebase/auth";
 
 const handleSignOut = () => {
   
-  console.log("working")
   const auth = getAuth();
-  console.log(auth.signOut())
   auth.signOut();
   localStorage.removeItem("userIdentification");
 
