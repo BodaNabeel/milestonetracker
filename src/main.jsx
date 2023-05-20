3b8572650de1e6a423d1733d9987ae410fd543a7
Nabeel Boda <needtoexplore01@gmail.com>
2023-05-20 12:35:09 +0530

    made the rotation text responsive

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { DataProvider } from "./data/DataContext";
 import App from "./App";
-import "./style.css";
+import "./style.css"
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
