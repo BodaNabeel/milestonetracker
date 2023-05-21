038544b2e8f9a791944e1e399bde5db76e20cb91
Nabeel Boda <needtoexplore01@gmail.com>
2023-05-21 13:40:12 +0530

    try as a guest feature enabled

--- src/component/LogInFunction.jsx
+++ src/component/LogInFunction.jsx
@@ -49,18 +49,35 @@ export default function LogInFunction() {
     signInWithPopup(auth, provider);
     localStorage.setItem("sign_in_clicked", true);
   };
+  const signInAsGuest = () => {
+    // userAuthenticationId(true)
+    setUserAuthenticationId(true)
+  }
 
   if (localStorage.getItem("sign_in_clicked")) {
     return (
+
       <button className="signin-btn-clicked">
         loading <ClipLoader color="white" size={10} />
       </button>
     );
   } else {
     return (
+      <div 
+      style={{
+        display:"flex",
+        gap: "20px"
+      }}
+      className="btn-container">
+      
       <button className="signin-btn" onClick={handleSignIn}>
         sign in now...!
       </button>
+
+      <button className="signin-btn" onClick={signInAsGuest}>
+        try as guest
+      </button>
+      </div>
     );
   }
   // return (
