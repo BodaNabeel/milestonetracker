2e85f5088e30007097bed2e047a88d23d131f309
Nabeel Boda <83013781+BodaNabeel@users.noreply.github.com>
2023-08-22 20:41:34 +0530

    removed sign in as guest

--- src/component/LogInFunction.jsx
+++ src/component/LogInFunction.jsx
@@ -32,14 +32,13 @@ export default function LogInFunction() {
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
-     
+      console.log(user);
       localStorage.setItem("userIdentification", user.uid);
       setUserAuthenticationId(
         localStorage.getItem("userIdentification") || null
       );
       writeUserData(user.displayName, user.email, user.uid);
     } else {
-      
       setUserAuthenticationId(null);
       localStorage.removeItem("userIdentification");
       localStorage.removeItem("sign_in_clicked");
@@ -49,42 +48,18 @@ export default function LogInFunction() {
     signInWithPopup(auth, provider);
     localStorage.setItem("sign_in_clicked", true);
   };
-  const signInAsGuest = () => {
-    // userAuthenticationId(true)
-    setUserAuthenticationId(true)
-  }
 
   if (localStorage.getItem("sign_in_clicked")) {
     return (
-
       <button className="signin-btn-clicked">
         loading <ClipLoader color="white" size={10} />
       </button>
     );
   } else {
     return (
-      <div 
-      style={{
-        display:"flex",
-        gap: "20px"
-      }}
-      className="btn-container">
-      
       <button className="signin-btn" onClick={handleSignIn}>
         sign in now...!
       </button>
-
-      <button className="signin-btn" onClick={signInAsGuest}>
-        try as guest
-      </button>
-      </div>
     );
   }
-  // return (
-  //   {localStorage.getItem("sign_in_clicked") ? <button></button> : <button> </button>}
-  // <button className="signin-btn" onClick={handleSignIn}>
-  //    sign in now...!
-
-  // </button>
-  // );
 }
